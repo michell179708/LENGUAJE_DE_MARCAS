f30eac4502bd0ccfff0d6788568406cb168ed0b2
Michell Herrera <her20045@byui.edu>
2023-12-15 23:42:31 +0100

    xml

---
+++ jason/tareaAprendizaje.js
@@ -0,0 +1,36 @@
+{
+    "Eventos":[
+        {
+            "EventoUno": "Primera Excursion",
+            "Nombre": "Se feliz escalando",
+            "Dificultad": "Medio",
+            "Desnivel": "560 x 20 cm",
+            "Distancia": "4 metros de largo",
+            "Tiempo": "20 minutos",
+            "Dia": "20 de Diciembre del 2023",
+            "Lugar": "Algorta",
+            "Hora": "10 am ",
+            "Precio": "100 euros",
+            "Plazas": "30",
+
+
+        },
+
+        {
+            "EventoDos": "Segunda Excursion",
+            "Nombre": "Se feliz caminando",
+            "Dificultad": 
+            "Desnivel": "",
+            "Distancia": "4 metros de largo",
+            "Tiempo": "20 minutos",
+            "Dia": "20 de Diciembre del 2023",
+            "Lugar": "Algorta",
+            "Hora": "10 am ",
+            "Precio": "100 euros",
+            "Plazas": "30",
+
+
+        },
+        
+    ]
+}
